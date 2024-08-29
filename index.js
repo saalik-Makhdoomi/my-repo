63264565fdbed0502f6a8dc055c78699e90990bd
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-08-29 13:53:26 +0530

    fixes

--- index.js
+++ index.js
@@ -3,39 +3,36 @@ const app = express();
 const path = require("path");
 const xhbs = require("express-handlebars");
 
-
-
 // middleware:
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // setting hbs enigine:
-app.engine("hbs" , xhbs.engine({
-    extname: "hbs",     // engine
-    defaultLayout: "layout",   // layout is the main page 
+app.engine(
+  "hbs",
+  xhbs.engine({
+    extname: "hbs", // engine
+    defaultLayout: "layout", // layout is the main page
     layoutsDir: path.join(__dirname, "views", "layout"),
-    partialsDir: path.join(__dirname, "views" , "partials"),
-  }))
-  
-  app.set("view engine", "hbs");
-  app.set("views" , path.join(__dirname , "views" , "pages"))
-  
+    partialsDir: path.join(__dirname, "views", "partials"),
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views", "pages"));
 
 //api routes:
 
-app.get('/', (req,res) => {
-    res.send("Hello this is browser");
+app.get("/", (req, res) => {
+  res.send("Hello this is browser");
 });
 
-app.get('/server',(req,res) => {
-    res.render("index")
+app.get("/index", (req, res) => {
+  res.render("index");
 });
-app.get('/contact',(req,res) => {
-    res.render("contact")
-})
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(3000, () => {
-    console.log("server started at port no.3000");
+  console.log("server started at port no.3000");
 });
